a917dad9d1dcfeeeb2e320d8794b389533a6023a
gasolin <gasolin@gmail.com>
2016-08-20 12:04:22 +0800

    add map

--- saihubot-diagnostics.js
+++ saihubot-diagnostics.js
@@ -1,15 +1,15 @@
 'use strict';
 // addon that provide confirm dialog function
 document.addEventListener('DOMContentLoaded', function() {
-  HuohuBot.prototype.responses.push(
+  SaihuBot.prototype.responses.push(
     { name: 'ping', rule: /PING$/i, action: function(robot, msg) {
       robot.send('PONG');
     }});
-  HuohuBot.prototype.responses.push(
+  SaihuBot.prototype.responses.push(
     { name: 'echo [string]', rule: /ECHO (.*)$/i, action: function(robot, msg) {
       robot.send(msg);
     }});
-  HuohuBot.prototype.responses.push(
+  SaihuBot.prototype.responses.push(
     { name: 'time', rule: /TIME$/i, action: function(robot, msg) {
       robot.send('Device time is ' + new Date());
     }});
